f77a832ecc8c2e6489d36e730ce750eaecd080c7
винтр <wintrforwork@gmail.com>
2025-03-26 01:41:00 +0300

    basic setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+
+
+export default {
+    providers: [
+      {
+        domain: "https://gentle-airedale-8.clerk.accounts.dev ",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvied from "@/components/providers/ConvexClientProvied";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Code Snippets",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ConvexClientProvied>{children}</ConvexClientProvied>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
